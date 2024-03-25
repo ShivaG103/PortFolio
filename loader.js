f5929c6e5d1451d3afdea4a4d52ad0be8bced6ed
ShivaG103 <shvm5454@gmail.com>
2024-03-25 20:41:40 +0530

    updated website link

--- loader.js
+++ loader.js
@@ -1,5 +1,7 @@
 'use client'
  
 export default function myImageLoader({ src, width, quality }) {
-  return `https://shivam-portfolio.vercel.app/${src}?w=${width}&q=${quality || 75}`
+  return `https://shivam-gupta-portfolio.vercel.app/${src}?w=${width}&q=${
+    quality || 75
+  }`;
 }
